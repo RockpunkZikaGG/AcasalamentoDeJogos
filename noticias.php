4ee001c89d7042cee33801189fbb41c21f85c56a
RockpunkZikaGG <william.rocha.silva.wrs@gmail.com>
2019-11-05 09:37:15 -0300

    diferenciado

--- noticias.php
+++ noticias.php
@@ -1,6 +1,32 @@
 <div class="container noticias">
     <?php
         include('configs.php');
+        if(isset($_COOKIE["CDJ_ADM"]) and $_COOKIE["CDJ_ADM"]!=""){
+            echo '
+                <div class="EditAmd">
+                    <form action="?page=Confirm2" method="post" enctype="multipart/form-data">
+                        <table>
+                            <tr>
+                                <td><p>Nome:</p></td><td><input type="text" name="nome"/></td>
+                            </tr>
+                            <tr>
+                                <td><p>Título:</p></td><td><input type="text" name="titulo"/></td>
+                            </tr>
+                            <tr>
+                                <td><p>Descrição:</p></td><td><input type="text" name="descricao"/></td>
+                            </tr>
+                            <tr>
+                                <td><p>Foto:</p></td><td><input id="inpFoto" class="inpFile" type="file" name="foto" /><label class="lbFoto" for="inpFoto">SELECIONE UMA FOTO</label></td>
+                            </tr>
+                        </table>
+                        <div class="buttons">
+                            <input class="btn" type="submit" name="opt" value="Incluir" />
+                            <input class="btn" type="reset" name="opt" value="Limpar"/>
+                        </div>
+                    </form>
+                </div>
+            ';
+        }
         if ($result = mysqli_query($conn, 'SELECT * FROM noticia')) {
             $row = mysqli_num_rows($result);
             if(mysqli_num_rows($result)>0){
