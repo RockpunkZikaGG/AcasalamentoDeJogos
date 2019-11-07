d84c814fbca46752e8a5229515c802d274826c10
FaraoAten <atenfernandes@gmail.com>
2019-11-06 22:43:46 -0200

    Deu um probleminha no CSS

--- home.php
+++ home.php
@@ -9,10 +9,10 @@
     </div>
 	<div class="row">
 	<div class="conteudo">
-	    <h1>CDJ IFSul</h1>
-        <p>No Clube de Jogos IFSul estimulamos diversas competências importantes para os estudos enquanto desplugamos do mundo virtual e saímos do stress do dia a dia 
-		jogando RPGs, jogos de cartas e tabuleiro modernos em um ambiente descontraído e acolhedor!</p> 
+	    <h1>Entre Cartas e Peças: aliando lazer e aprendizagens</h1>
+        <p>É um projeto voltado para alunos da rede pública onde aplicamos os principios do CDJ IFSul, visando expandir 
+		essa experiência à eles e permitir que tenham contato com um novo mundo!</p> 
 	</div>
-	<img src="Source/Images/r1.jpg"/>
+	<img src="Source/Images/entre cartas e peças.jpg"/>
     </div>
 </div>
