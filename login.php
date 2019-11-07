1e12cfeb412f4f360f6892267b7651bfd27a79bb
RockpunkZikaGG <william.rocha.silva.wrs@gmail.com>
2019-11-07 09:15:24 -0300

    Save

--- login.php
+++ login.php
@@ -2,9 +2,9 @@
 <fieldset>
 	<legend>Login Administrador CDJ</legend>
 	<form action="?page=Validacao" method="post" enctype="multipart/form-data">
-		<p>Email:<input type="email" name="email" maxlenght=100 size=100 placeholder="E-mail"/></p>
-		<p>Senha:<input type="password" name="senha" maxlenght=20 size=20 placeholder="Senha"/></p>
-		<input type="submit" name="Logar" value="Logar" />
+		<p>Email:<input type="email" name="email" placeholder="E-mail"/></p>
+		<p>Senha:<input type="password" name="senha" placeholder="Senha"/></p>
+		<input class="btn" type="submit" name="Logar" value="Logar" />
 	</form>
 </fieldset>
  </div>
